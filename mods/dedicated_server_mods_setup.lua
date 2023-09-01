3510336e26a9d7f99a9b5691e3ba20f99e7c218c
lzp <1245491965@qq.com>
2023-09-01 22:26:27 +0800

    海难新存档提交

--- mods/dedicated_server_mods_setup.lua
+++ mods/dedicated_server_mods_setup.lua
@@ -9,3 +9,17 @@
 	--The Workshop id can be found at the end of the url to the collection's Workshop page.
 	--Example: http://steamcommunity.com/sharedfiles/filedetails/?id=379114180
 	--ServerModCollectionSetup("379114180")
+ServerModSetup("350811795")
+ServerModCollectionSetup("1378549454")
+ServerModSetup("1467214795")
+ServerModSetup("1530801499")
+ServerModSetup("1751811434")
+ServerModSetup("2048852282")
+ServerModSetup("2287303119")
+ServerModSetup("2373346252")
+ServerModSetup("2505341606")
+ServerModSetup("2771698903")
+ServerModSetup("378160973")
+ServerModSetup("831523966")
+ServerModSetup("850518166")
+ServerModSetup("949808360")

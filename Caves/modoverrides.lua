d4c4729985a22b0ed2e0dd27a85198e338665ae6
lzp <1245491965@qq.com>
2023-09-10 01:25:14 +0800

    重新加入应该开的mod

--- Caves/modoverrides.lua
+++ Caves/modoverrides.lua
@@ -43,5 +43,27 @@ return {
       ["生物改变"]=false 
     },
     enabled=true 
-  } 
+  },
+   -- 一秒5捡
+    ["workshop-850518166"] = { enabled = true },
+    -- 999堆叠
+    ["workshop-831523966"] = { enabled = true },
+    -- 防卡-改
+    ["workshop-2505341606"] = { enabled = true },
+    -- 5格装备
+    ["workshop-2373346252"] = { enabled = true },
+    -- 快速砍树
+    ["workshop-1751811434"] = { enabled = true },
+    -- 全球定位
+    ["workshop-378160973"] = { enabled = true },
+    -- 木牌传送
+    ["workshop-2119742489"] = { enabled = true },
+    -- 保温石无耐久
+    ["workshop-466732225"] = { enabled = true },
+    -- Show Me(中文)
+    ["workshop-2287303119"] = { enabled = true },
+    -- 中文语言包
+    ["workshop-1301033176"] = { enabled = true },
+    -- 重生
+    ["workshop-1301033176"] = { enabled = true }
 }
